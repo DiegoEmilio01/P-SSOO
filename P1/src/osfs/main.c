36ec6d9d0cb974460c2396a77d49c9f045c55490
DiegoEmilio01 <diegobustama@uc.cl>
2021-05-31 20:42:43 -0400

    Update readme, fix mem leaks and write new partition in the disk mbt

--- P1/src/osfs/main.c
+++ P1/src/osfs/main.c
@@ -4,21 +4,22 @@
 #include "../structs/structs.h"
 #include "../structs/os_API.h"
 
-Mbt* mbt;
 
 int main(int argc, char **argv)
 {
-  mbt = malloc(sizeof(Mbt));
   os_mount(argv[1], 1);
   
   os_mbt();
 
-  os_create_partition(5, 20000);
-
-  for (int i = 0; i < mbt->entry_quantity; i++)
-    if (mbt->entry_container[i])
-      free(mbt->entry_container[i]);
-  free(mbt->entry_container);
-  free(mbt);
+  os_create_partition(5, 20000);    // 123506, 20000
+  
+  os_mount(argv[1], 1);
+  os_mbt();
+  
+  os_delete_partition(5);
+  os_mount(argv[1], 1);
+  os_mbt();
+  
+  destroy_mbt();
   return 0;
 }
--- P1/src/structs/os_API.c
+++ P1/src/structs/os_API.c
@@ -9,12 +9,17 @@
 
 char* path_disk;
 int partition;
+Mbt* mbt;
 
 /** monta el os
  * @param
  * @returns Mbt*
 */
 void os_mount(char* diskname, int partition_id){
+  if (mbt){
+    destroy_mbt();
+  }
+  mbt = malloc(sizeof(Mbt));
 
   path_disk = diskname;
   partition = partition_id;
@@ -50,18 +55,18 @@ void os_mbt(){
 }
 
 void os_create_partition(int id, int size){
-  printf("Creando partición %d de tamaño %d\n", id, size);
+  printf("Creando partición %d de tamaño %d\n\n", id, size);
   if (size < 16384 || size > 131072)
   {
-    printf("ERROR: wrong size as argument. Size must be between 16384 and 131072.\n");
+    printf("ERROR: wrong size as argument. Size must be between 16384 and 131072.\n\n");
   }
   else if (id < 0 || id > 128)
   {
-    printf("ERROR: wrong id as argument. Id must be between 0 and 128.\n");
+    printf("ERROR: wrong id as argument. Id must be between 0 and 128.\n\n");
   }
   else if (mbt->entry_container[id] && mbt->entry_container[id]->is_valid)
   {
-    printf("ERROR: partition already created. If aditional partition is needed, please try another id.\n");
+    printf("ERROR: partition already created. If aditional partition is needed, please try another id.\n\n");
   }
   else
   {
@@ -71,18 +76,13 @@ void os_create_partition(int id, int size){
         tentry = init_tentry(tentry, mbt->entry_container[entry_id]->location, mbt->entry_container[entry_id]->size);
       }
     }
-    // TEntry* current = tentry;
-    // while (current)
-    // {
-    //   printf("%" PRIu32 ", %" PRIu32 "\n", current->location, current->size);
-    //   current = current->next;
-    // }
-    
+
     if (!tentry || size < tentry->location)
     {
       Entry* entry = init_entry(true, id, 0, size);
       mbt->entry_container[id] = entry;
-      printf("Partition created successfully.\n");
+      write_new_partition(id, 0, size);
+      printf("Partition created successfully.\n\n");
       destroy_tentry(tentry);
       return;
     }
@@ -97,11 +97,12 @@ void os_create_partition(int id, int size){
         {
           Entry* entry = init_entry(true, id, posible_location, size);
           mbt->entry_container[id] = entry;
-          printf("Partition created successfully.\n");
+          write_new_partition(id, posible_location, size);
+          printf("Partition created successfully.\n\n");
         }
         else
         {
-          printf("ERROR: partition cannot be allocated, please try a smaller size.\n");
+          printf("ERROR: partition cannot be allocated, please try a smaller size.\n\n");
         }
         destroy_tentry(tentry);
         return;
@@ -110,7 +111,8 @@ void os_create_partition(int id, int size){
       {
         Entry* entry = init_entry(true, id, posible_location, size);
         mbt->entry_container[id] = entry;
-        printf("Partition created successfully.\n");
+        write_new_partition(id, posible_location, size);
+        printf("Partition created successfully.\n\n");
         destroy_tentry(tentry);
         return;
       }
@@ -119,9 +121,32 @@ void os_create_partition(int id, int size){
   }
 }
 
+void write_new_partition(int id, int location, int size) // falta crear los bloques de bitmap
+{
+  FILE* disk = fopen(path_disk, "r+b");
+  fseek(disk, 0, SEEK_SET);
+  uint8_t buffer[8];
+  for (int entry_id = 0; entry_id < mbt->entry_quantity; entry_id++)
+  {
+    size_t ret = fread(buffer, sizeof(uint8_t), (size_t)8, disk);
+    bool is_valid = bt_get(buffer, 0);
+    if (!is_valid)
+    {
+      fseek(disk, -8, SEEK_CUR);
+      uint8_t new_buffer[8];
+      new_buffer[0] = bit_and_id_to_byte(1, (uint8_t)id);
+      insert_location_to_buffer(new_buffer, (uint32_t)location);
+      insert_size_to_buffer(new_buffer, (uint32_t)size);
+      fwrite(new_buffer, sizeof(uint8_t), 8, disk);
+      break;
+    }
+  }
+  fclose(disk);
+}
+
 // TODO: lanzar OS_ERROR invalid_delete_partition
 void os_delete_partition(int delete_id){
-  printf("Eliminando partición %d de la MBT\n", delete_id);
+  printf("Eliminando partición %d de la MBT\n\n", delete_id);
 
   // Crea el puntero al archivo disk, en modo read write 
   FILE* disk = fopen(path_disk, "r+b");
@@ -132,7 +157,7 @@ void os_delete_partition(int delete_id){
   for (int entry_id = 0; entry_id < mbt->entry_quantity; entry_id++){
 
     // Obtenemos el id de partición y el bit de validez
-    size_t ret = fread(buffer, sizeof(uint8_t), (size_t)8, disk);
+    size_t ret = fread(buffer, sizeof(uint8_t), (size_t)1, disk);
     bool is_valid = bt_get(buffer, 0);
     int partition_id = get_partition_id(buffer[0]);
 
@@ -140,6 +165,7 @@ void os_delete_partition(int delete_id){
       // si la partición es válida e id es correcto, cambia el primer byte de la entrada a 0.
       // es decir, el bit de validez y el id pasa a ser 0.
       uint8_t zero_buffer[1] = {0};
+      fseek(disk, -1, SEEK_CUR);
       fwrite(zero_buffer, sizeof(uint8_t), 1, disk);
       mbt->entry_container[entry_id]->is_valid = 0;
 
@@ -147,18 +173,17 @@ void os_delete_partition(int delete_id){
       break;
     }
     // Avanzamos hacia el siguiente trozo de 8 bytes, solución pobre
-    fseek(disk, 8 * (entry_id - 1), SEEK_SET);
+    fseek(disk, 8 * (entry_id + 1), SEEK_SET);
   }
 
   if (raise_error){
     // RAISE ERROR
   }
-
   fclose(disk);
 }
 
 void os_reset_mbt(){
-  printf("Eliminando particiones de la MBT\n");
+  printf("Eliminando particiones de la MBT\n\n");
 
   // Crea el puntero al archivo disk, en modo read write 
   FILE* disk = fopen(path_disk, "r+b");
@@ -173,7 +198,7 @@ void os_reset_mbt(){
     mbt->entry_container[entry_id]->is_valid = 0;
     // No encontré si fwrite mueve el puntero en ningún lado :( p.d mi intuición dice que sí lo mueve
     // asi que tenemos la solución pobre:
-    fseek(disk, 8 * (entry_id - 1), SEEK_SET);
+    fseek(disk, 8 * (entry_id + 1), SEEK_SET);
   }
 
   // tal vez un raise_error pero no se me ocurre qué.
@@ -183,21 +208,21 @@ void os_reset_mbt(){
 }
 
 void os_open(char* filename, char mode){
-  printf("Abriendo archivo %s\n", filename);
+  printf("Abriendo archivo %s\n\n", filename);
 }
 
 void os_read(osFile* file_desc, void* buffer, int nbytes){
-  printf("Leyendo %d bytes del archivo\n", nbytes);
+  printf("Leyendo %d bytes del archivo\n\n", nbytes);
 }
 
 void os_write(osFile* file_desc, void* buffer, int nbytes){
-  printf("Escribiendo archivo\n");
+  printf("Escribiendo archivo\n\n");
 }
 
 void os_close(osFile* file_desc){
-  printf("Cerrando archivo\n");
+  printf("Cerrando archivo\n\n");
 }
 
 void os_rm(char* filename){
-  printf("Borrando archivo %s\n", filename);
+  printf("Borrando archivo %s\n\n", filename);
 }
